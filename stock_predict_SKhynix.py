f033f6110c910badea0357669b9e08b76d960826
jjony553 <55618626+jjony553@users.noreply.github.com>
2019-12-29 20:07:44 +0900

    Add files via upload

---
+++ stock_predict_SKhynix.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense, Activation
+# from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau
+import datetime
+
+#Load Dataset
+data = pd.read_csv(r'C:\Users\whdgu\Desktop\stocks\dataset\sk.KS.csv')
+data.head()
+
+#Compute Mid Price
+high_prices = data['High'].values
+low_prices = data['Low'].values
+mid_prices = (high_prices + low_prices) / 2
+
+#Create Windows
+seq_len = 50 #최근 50일간의 데이터로 내일 예측, 50개로 묶고 다음 날(51일) 예측
+sequence_length = seq_len + 1
+
+result = []
+for index in range(len(mid_prices) - sequence_length):
+    result.append(mid_prices[index: index + sequence_length])
+
+#Normalize Data
+normalized_data = []
+for window in result:
+    normalized_window = [((float(p) / float(window[0])) - 1) for p in window]
+    normalized_data.append(normalized_window)
+
+result = np.array(normalized_data)
+
+## train data와 test data를 9:1로 나눔
+row = int(round(result.shape[0] * 0.9))
+train = result[:row, :]
+np.random.shuffle(train)
+
+x_train = train[:, :-1]
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+y_train = train[:, -1]
+
+x_test = result[row:, :-1]
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+y_test = result[row:, -1]
+
+x_train.shape, x_test.shape
+
+#Build a Model
+model = Sequential()
+
+model.add(LSTM(50, return_sequences=True, input_shape=(50, 1)))
+
+model.add(LSTM(64, return_sequences=False))
+
+model.add(Dense(1, activation='linear'))
+
+model.compile(loss='mse', optimizer='rmsprop')
+
+model.summary()
+
+#Training
+##데이터에 NULL이 있을시 NULL 삭제 후 학습
+model.fit(x_train, y_train,
+    validation_data=(x_test, y_test),
+    batch_size=10,
+    epochs=20)
+
+#Prediction, Make a graph
+pred = model.predict(x_test)
+
+fig = plt.figure(facecolor='white', figsize=(20, 10))
+ax = fig.add_subplot(111)
+ax.plot(y_test, label='True')
+ax.plot(pred, label='Prediction')
+ax.legend()
+plt.show()
